cde629eaac88b8ce3613a778beab8f3d551f2eb0
JeanTheBaptist <84333282+JeanTheBaptist@users.noreply.github.com>
2022-09-06 09:00:41 +0800

    Create Q1EX1.java

---
+++ Q1EX1.java
@@ -0,0 +1,41 @@
+public class Mu06_Cristobal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        var studentOne = "Brent";
+        var studentTwo = "JD";
+        var studentThr = "Ivan";
+        
+        double gradeOne = 2.00;
+        double gradeTwo = 1.25;
+        double gradeThr = 1.50;
+        
+        var laneOne = "Solo";
+        var laneTwo = "Jungle";
+        var laneThr = "Duo";
+        
+        int hoursPlayedOne = 5;
+        int hoursPlayedTwo = 1;
+        int hoursPlayedThr = 3;
+        
+        System.out.println("Student 1");
+        System.out.println("Name: " + studentOne);
+        System.out.println("Wild Rift Lane: " + laneOne);
+        System.out.println("Average Hours Played a Day: " + hoursPlayedOne);
+        System.out.println("");
+        
+        System.out.println("Student 2");
+        System.out.println("Name: " + studentTwo);
+        System.out.println("Wild Rift Lane: " + laneTwo);
+        System.out.println("Average Hours Played a Day: " + hoursPlayedTwo);
+        System.out.println("");
+        
+        System.out.println("Student 3");
+        System.out.println("Name: " + studentThr);
+        System.out.println("Wild Rift Lane: " + laneThr);
+        System.out.println("Average Hours Played a Day: " + hoursPlayedThr);
+    }
+    
+}
