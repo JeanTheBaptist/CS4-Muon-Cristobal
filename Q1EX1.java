b6367970908d9ae14dde0555351261d4bb32e4a6
JeanTheBaptist <84333282+JeanTheBaptist@users.noreply.github.com>
2022-09-08 11:23:16 +0800

    Update Q1EX1.java

--- Q1EX1.java
+++ Q1EX1.java
@@ -24,18 +24,41 @@ public class Mu06_Cristobal {
         System.out.println("Name: " + studentOne);
         System.out.println("Wild Rift Lane: " + laneOne);
         System.out.println("Average Hours Played a Day: " + hoursPlayedOne);
+        System.out.println("GWA: " + gradeOne);
         System.out.println("");
         
         System.out.println("Student 2");
         System.out.println("Name: " + studentTwo);
         System.out.println("Wild Rift Lane: " + laneTwo);
         System.out.println("Average Hours Played a Day: " + hoursPlayedTwo);
+        System.out.println("GWA: " + gradeTwo);
         System.out.println("");
         
         System.out.println("Student 3");
         System.out.println("Name: " + studentThr);
         System.out.println("Wild Rift Lane: " + laneThr);
         System.out.println("Average Hours Played a Day: " + hoursPlayedThr);
+        System.out.println("GWA: " + gradeThr);
+        System.out.println("");
+        
+        double averageGrade = (gradeOne + gradeTwo + gradeThr)/3;
+        System.out.println("Their average grade is " + averageGrade);
+        
+        var laneCheck = "";
+        
+        if ((laneOne != laneThr) && (laneThr != laneTwo) && (laneTwo != laneOne)){
+            laneCheck = "No";
+        }
+        
+        else{
+            laneCheck = "Yes"; 
+        }
+        
+        System.out.println("Are there any repeated lanes: " + laneCheck);
+        
+        int hoursPlayedTotal = hoursPlayedOne + hoursPlayedTwo + hoursPlayedThr;
+        System.out.println("They play a total of " + hoursPlayedTotal + " hours everyday." );
+        }
     }
     
-}
+
